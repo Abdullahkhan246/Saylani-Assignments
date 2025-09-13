3b95fa6461e86411cf322357951d5ddaad89b6ea
Abdullahkhan246 <anittawilson50@gmail.com>
2025-09-13 12:33:22 +0500

    program change

--- Assignment 7/program-1/script.js
+++ Assignment 7/program-1/script.js
@@ -1,11 +1,30 @@
 //1. program to check whether the light is on or off
-var input= prompt("Enter a number either 0 or 1");
 
-if(input==0){
-    console.log("The light is off");
-}
-else if(input==1){
-    console.log("The light is on"); 
-}else{
-    console.log("Invalid input");
+
+
+while (true) {
+  let userInput = prompt("Enter a number (0 for OFF, 1 for ON)");
+
+  if (userInput === null || userInput.trim() === "") {
+    alert("Invalid input. Try again!");
+    continue;
+  }
+
+  
+
+  if (isNaN(userInput)) {
+    alert("Invalid input. Try again!");
+    continue;
+  }
+let num = Number(userInput);
+  if (num === 0) {
+    alert("The light is OFF");
+  } else if (num === 1) {
+    alert("The light is ON");
+  } else {
+    alert("Invalid input. Please enter only 0 or 1.");
+    continue;
+  }
+
+  break;
 }
